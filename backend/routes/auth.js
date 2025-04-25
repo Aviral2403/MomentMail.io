7dae6c336f71d3448a191b56dc22d6cdccd43b2d
Aviral2403 <aviralkhandelwal03@gmail.com>
2025-04-25 11:11:19 +0530

    Remove all files causing merge conflicts

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { googleAuth } = require("../controllers/authControllers");
-const { connectGoogleDrive } = require("../controllers/driveControllers");
-const router = express.Router();
-
-router.get('/google', googleAuth);
-router.get('/connect-drive', connectGoogleDrive);
-
-module.exports = router;
